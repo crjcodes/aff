66572c195bb466200bf11df2404e51c876322264
crjcodes <crjcodes@users.noreply.github.com>
2020-04-23 14:06:15 -0500

    added validation for keyword references

--- app/models/key_ref_map.rb
+++ app/models/key_ref_map.rb
@@ -1,3 +1,81 @@
 class KeyRefMap < ApplicationRecord
   belongs_to :keyword
+
+  validates :keyword, presence: true
+  validates :book, :chapter, :verse_start, presence: true
+
+  validates :book, format: /\A[\sa-z0-9 ]+\Z/i
+  validates :book, inclusion: { in: 
+    [
+      "Genesis",
+      "Exodus",
+      "Leviticus",
+      "Numbers",
+      "Deuteronomy",
+      "Joshua",
+      "Judges",
+      "Ruth",
+      "1st Samuel",
+      "2nd Samuel",
+      "1st Kings",
+      "2nd Kings",
+      "1st Chronicles",
+      "2nd Chronicles",
+      "Ezra",
+      "Nehemiah",
+      "Esther",
+      "Job",
+      "Psalms",
+      "Proverbs",
+      "Ecclesiastes",
+      "Song of Solomon",
+      "Isaiah",
+      "Jeremiah",
+      "Lamentations",
+      "Ezekiel",
+      "Daniel",
+      "Hosea",
+      "Joel",
+      "Amos",
+      "Obadiah",
+      "Jonah",
+      "Micah",
+      "Nahum",
+      "Habakkuk",
+      "Zephaniah",
+      "Haggai",
+      "Zechariah",
+      "Malachi",
+      "Matthew",
+      "Mark",
+      "Luke",
+      "John",
+      "Acts",
+      "Romans",
+      "1 Corinthians",
+      "2 Corinthians",
+      "Galatians",
+      "Ephesians",
+      "Philippians",
+      "Colossians",
+      "1 Thessalonians",
+      "2 Thessalonians",
+      "1 Timothy",
+      "2 Timothy",
+      "Titus",
+      "Philemon",
+      "Hebrews",
+      "James",
+      "1 Peter",
+      "2 Peter",
+      "1 John",
+      "2 John",
+      "3 John",
+      "Jude",
+      "Revelation"
+    ]
+  }
+
+  validates :chapter, :verse_start, numericality: { only_integer: true }
+
 end
