3946f2c14c642f9d154be1acba8fac95264eb771
crjcodes <crjcodes@users.noreply.github.com>
2020-04-23 14:06:27 -0500

    added validation for keywords

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,7 @@
 class Keyword < ApplicationRecord
+    validates :kw, presence: true
+
+    validates :kw, length: { minimum: 2 }
+    validates :kw, length: { maximum: 30, too_long: "%{count} characters is the maximum allowed" }
+    validates :kw, format: /\A[\sa-z ]+\Z/i
 end
